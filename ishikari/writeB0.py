f2674a9378b351681da8805a04031d5922e52a67
Emily Middleton <emiddleton@smith.edu>
2025-07-16 11:56:18 -0400

    modified to reflect new scratch and output directory locations

---
+++ ishikari/writeB0.py
@@ -0,0 +1,24 @@
+import os
+from pylab import *
+from clawpack.geoclaw import fgmax_tools
+
+try:
+    CLAW = os.environ['CLAW']
+except:
+    raise Exception("*** Must first set CLAW enviornment variable")
+
+# Scratch directory for storing topo and dtopo files:
+dir = '/Users/anitamiddleton/Documents/python/tsunami_proj'
+
+outdir = os.path.join(dir, 'outputs/ishikari/_output')
+print('Using output from outdir = ', outdir)
+# Read fgmax data:
+fg = fgmax_tools.FGmaxGrid()
+fgmax_input_file_name = outdir + '/fgmax_grids.data'
+print('fgmax input file: \n  %s' % fgmax_input_file_name)
+fg.read_fgmax_grids_data(fgno=1, data_file=fgmax_input_file_name)
+fg.read_output(outdir=outdir)
+B0 = where(fg.B > -1e9, fg.B, -9999.)
+fname = os.path.join(dir, 'scratch/ishikari/ishikari_B0.txt')
+savetxt(fname, B0, fmt='%.3f')
+print('saved %s' % fname)
